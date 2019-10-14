be8381e04c2f65ea8d397fa884648740b1e04b00
Ozan <bakonong99@gmail.com>
2019-10-15 00:32:49 +0700

    Menambahkan fungsi tambah data dan hapus data

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -6,6 +6,7 @@
 		{
 			parent::__construct();
 			$this->load->model('Mahasiswa_model');
+			$this->load->library('form_validation');
 		}
 
 		public function index()
@@ -17,4 +18,28 @@
 			$this->load->view('templates/footer');
 		}
 
+		public function tambah()
+		{
+			$data['judul'] = 'Form Tambah Data Mahasiswa';
+
+			$this->form_validation->set_rules('nama', 'Nama', 'required');
+			$this->form_validation->set_rules('nrp', 'NRP', 'required|numeric');
+			$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+			if( $this->form_validation->run() == FALSE ) {
+			$this->load->view('templates/header' , $data);
+			$this->load->view('mahasiswa/tambah');
+			$this->load->view('templates/footer');
+			} else {
+				$this->Mahasiswa_model->tambahDataMahasiswa();
+				$this->session->set_flashdata('flash', 'Ditambahkan');
+				redirect('mahasiswa');
+			}
+		}
+
+		public function hapus($id)
+		{
+			$this->Mahasiswa_model->hapusDataMahasiswa($id);
+			$this->session->set_flashdata('flash', 'Dihapus');
+			redirect('mahasiswa');
+		}
 	}
