d54f657caec805457b68290adcc64748388116ef
Ozan <bakonong99@gmail.com>
2019-10-20 23:28:48 +0700

    Menambahkan file pagination.php, People.php, Peoples_model.php, index.php di file views/peoples

--- application/controllers/People.php
+++ application/controllers/People.php
@@ -1,36 +1,40 @@
-<?php 
+<?php
 
-	class People extends CI_Controller{
+class People extends CI_Controller
+{
+    public function index()
+    {
+        $data['judul'] = 'List of People';
 
-		public function index()
-		{
-			$data['judul'] = 'List of Peoples';
+        $this->load->model('Peoples_model', 'peoples');
 
-			$this->load->model('Peoples_model', 'peoples');
+        //load library
+        $this->load->library('pagination');
 
-			$this->load->library('pagination');
+        //ambil data keyword
+        if($this->->post('submit')) {
+            $data['keyword'] = $this->input->post('keyword');
+            $this->session->set_userdata('keyword, $data['keyword);
 
-			if($this->input=>post('submit')) {
-				$data['keyword'] = $this->input->post('keyword');
-				$this->session=>set_userdata('keyword', $data['keyword']);
-			} else {
-				$data['keyword'] = $this=>session=>userdata('keyword');
-			}
+        } else {
+            $data['keyword'] = $this->session->userdata('keyword');
 
-			$this->db->like('nama', $data['keyword']);
-			$this->db->or_like('email', $data['keyword']);
-			$this->db->from('peoples');
-			$config['total_rows'] = $config['total_rows'];
-			$config['per_page'] = 8;
+        }
+        //config
+        $this->db->like('name', $data['keyword']);
+        $this->db->like('email', $data['keyword']);
+        $this->db->from('peoples');
+        $config['per_page'] = 8;
 
-			$this->pagination->initialize($config);
+        //initialize
+        $this->pagination->initialize($config);
 
-			$data['start'] = $this->uri->segment(3);
-			$data['peoples'] = $this->peoples->getPeoples($config['per_page'], $data['start'], $data['keyword']);
+        $data['start'] = $this->url->segment(3);
+        $data['peoples'] = $this->peoples->getPeoples($config['per_page'], $data['start'], $data['keyword']);
 
-			$this->load->view('templates/header', $data);
-			$this->load->view('peoples/index', $data);
-			$this->load->view('templates/footer');
-		}
+        $this->load->view('templates/header', $data);
+        $this->load->view('peoples/index', $data);
+        $this->load->view('templates/footer');
 
-	}
+    }
+}
